963a36f005d77934aff4310f7f30a41c3da71787
Lab1 <arjun1299@gmail.com>
2020-12-30 18:37:30 +0530

    Added a large status text box and added priming Tab

--- logic.py
+++ logic.py
@@ -4,7 +4,7 @@ import serial
 from basicui import Ui_MainWindow
 import serial.tools.list_ports
 from datetime import datetime as dt
-import connectTab
+import os
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -14,11 +14,27 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.ui.setupUi(self)
         self.port=""
         
+        self.statusTxt= self.ui.statusTxt
+
         self.init_connectTab()
 
         self.init_commandTab()
 
         self.init_recurringTab()
+
+        self.init_primingTab()
+        
+        self.rotations=0
+        self.ongoing= self.ui.tabWidget.tabText(self.ui.tabWidget.currentIndex())
+        
+        self.updateStatus()
+
+    def updateStatus(self):
+        self.statusTxt.clear()
+        self.statusTxt.appendPlainText("Reciever Status:" +  ("Connected" if self.isConnectedReciever() else "Disconnected" ))
+        self.statusTxt.appendPlainText("BLE Status:" + ("Connected" if self.isConnectedBLE() else "Disconnected") )
+        self.statusTxt.appendPlainText("Rotations: " + str(self.rotations))
+        self.statusTxt.appendPlainText("Ongoing:"+ self.ongoing)
 
     """
         Tab 1- connect tab
@@ -36,37 +52,103 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.scanButton.clicked.connect(self.scanPort)
         #connect button
         self.connectButton=self.ui.connectButton
+        self.connectButton.setEnabled(False)
         self.connectButton.clicked.connect(self.connectPort)
         
 
-    def isConnected(self):
-        if self.port=="":
-            return False
-        return True
+    def isConnectedBLE(self):
+        #check for heartbeat here
+        pass
+
+        return 0
+
+    def isConnectedReciever(self):
+        #checks if bluetooth reciever is connected
+        if(os.system("hcitool scan")== 256):
+            return 0
+        return 1
+
 
     def chk(self):
-        self.ui.tabWidget.setTabEnabled(1,self.isConnected())
-        self.ui.tabWidget.setTabEnabled(2,self.isConnected())
-    
+        #enable tabs
+        #self.ui.tabWidget.setTabEnabled(1,self.isConnectedBLE())
+        #self.ui.tabWidget.setTabEnabled(2,self.isConnectedBLE())
+        pass
+
+
     def connectPort(self):
         if(self.dropdown.currentText()):
             self.port=self.dropdown.currentText()
-            self.chk()
+            
+            """
+            #add logic to pair with BLE
+            """
+
             print("Connected to:"+self.dropdown.currentText())
     
     def scanPort(self):
         
         print("Scanning...")
+        
+        self.ongoing="Scanning"
+        self.updateStatus()
+        
+        
+        if(self.isConnectedReciever):
+            self.updateStatus()
+            return 0
+        
+        cmd=os.popen("hcitool scan")
+        
+        comlist = cmd.read()
+        
+        comlist = comlist.split('\n')
+        
+        #the last element is empty 
+        comlist.pop()
 
-        comlist = serial.tools.list_ports.comports()
         connected = []
         self.dropdown.clear()
         for element in comlist:
             connected.append(element.device)
             self.dropdown.addItem(element.device)
+        
+        self.connectButton.setEnabled(True)
 
     """
     Tab-2
+
+    """
+
+    def init_primingTab(self):
+        #start prime
+        self.startPrime=self.ui.startPrime
+        self.startPrime.clicked.connect(self.enablePriming)
+        #rotate
+        self.rotateBtn=self.ui.rotateBtn
+        self.rotateBtn.setEnabled(False)
+
+        #stop
+        self.stopBtn=self.ui.stopBtn
+        self.stopBtn.setEnabled(False)
+
+    def enablePriming(self):
+        self.rotateBtn.setEnabled(True)
+        self.stopBtn.setEnabled(True)
+        self.updateStatus()
+
+    def rotate(self):
+        #do one rotation
+        pass
+
+
+    def stop(self):
+        #do one rotation
+        pass
+    
+    """
+    Tab-3
+
     """
 
     def init_commandTab(self):
@@ -89,7 +171,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
 
     """
-    Tab-3
+    Tab-4
     """
 
     def init_recurringTab(self):
